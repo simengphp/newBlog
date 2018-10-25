c9ebebd20239f55bf4f7ac5bfdd96a262820c567
liuzhu <liuzhu@zanpu.com>
2018-10-25 16:55:18 +0800

    项目初始化

--- resources/views/website/common/right.blade.php
+++ resources/views/website/common/right.blade.php
@@ -6,7 +6,7 @@
     @endforeach
     <ul class="ad">
         <a href="/" target="_blank">
-            <img src="/uploads/{{$config->pic}}" width="260" style="padding-top: 20px">
+            <img src="/uploads/{{$config->pic}}" width="240" style="padding-top: 20px">
         </a>
     </ul>
 </div>
