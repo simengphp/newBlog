44e06fa4c006ecb3f0b32d405f6d9261eb81bef2
liuzhu <liuzhu@zanpu.com>
2018-10-25 15:44:42 +0800

    项目初始化

--- resources/views/website/common/header.blade.php
+++ resources/views/website/common/header.blade.php
@@ -6,16 +6,13 @@
     <meta name="keywords" content="{{$website_key}}"/>
     <meta name="description" content="{{$website_desc}}" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <link rel="stylesheet" href="./static/reset.css">
-    <link rel="stylesheet" href="./static/index.css">
-    <script src="{{ asset('./website/hm.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('./website/reset.css') }}">
     <script src="{{ asset('./website/jquery.js') }}"></script>
     <link href="{{ asset('./website/bootstrap.css') }}" rel="stylesheet">
     <script src="{{ asset('./website/bootstrap.js') }}"></script>
     <script src="{{ asset('./website/layer.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('./website/layer.css') }}" id="layuicss-layer">
     <link rel="stylesheet" href="{{ asset('./website/index.css') }}">
-    <script src="{{ asset('./website/utils.js') }}"></script>
-    <script src="{{ asset('./website/index.js') }}"></script>
+
 
 </head>
