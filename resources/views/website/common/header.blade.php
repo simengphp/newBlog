48289beb1ec0c23847caceb2e8fe4883a30c8778
476319748@qq.com <476319748@qq.com>
2018-10-27 15:53:21 +0800

    首页

--- resources/views/website/common/header.blade.php
+++ resources/views/website/common/header.blade.php
@@ -5,6 +5,7 @@
     <title>{{$website_title}}</title>
     <meta name="keywords" content="{{$website_key}}"/>
     <meta name="description" content="{{$website_desc}}" />
+    <link rel="icon" type="image/x-icon" href="favicon.ico"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <link rel="stylesheet" href="{{ asset('./website/reset.css') }}">
     <script src="{{ asset('./website/jquery.js') }}"></script>
